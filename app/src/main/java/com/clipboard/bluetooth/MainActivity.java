3f667dacc8d99c2b6cd1e7005396223ea0fc629f
Sb-Rahman <sb.rahman@yahoo.com>
2023-06-20 21:47:16 +0600

    speech to text added

--- app/src/main/java/com/clipboard/bluetooth/MainActivity.java
+++ app/src/main/java/com/clipboard/bluetooth/MainActivity.java
@@ -1,6 +1,8 @@
 package com.clipboard.bluetooth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
@@ -8,8 +10,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -55,5 +59,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something...");
+                try {
+                    startActivityForResult(intent, REQUEST_CODE);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Sorry your phone does not support Speech input", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQUEST_CODE: {
+                if (resultCode == RESULT_OK && data != null) {
+
+                    ArrayList<String> res = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    editText.setText(res.get(0));
+                    t1.speak(res.get(0), TextToSpeech.QUEUE_FLUSH, null);
+
+
+                }
+                break;
+            }
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        //shutdown
+        if (t1!=null){
+            t1.stop();
+            t1.shutdown();
+        }
+
+        super.onDestroy();
     }
 }
