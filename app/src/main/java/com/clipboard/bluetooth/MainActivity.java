17c8f8b733ebd64308b259509413ca21a35172ca
Sb-Rahman <sb.rahman@yahoo.com>
2023-06-20 21:26:46 +0600

    Text to speak added

--- app/src/main/java/com/clipboard/bluetooth/MainActivity.java
+++ app/src/main/java/com/clipboard/bluetooth/MainActivity.java
@@ -1,25 +1,26 @@
 package com.clipboard.bluetooth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-//declaration of global variables
+    //declaration of global variables
     Button button;
     EditText editText;
     ImageView mic;
 
     TextToSpeech t1;
     String userToSpeak;
-    private final int REQUEST_CODE=100;
+    private final int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +28,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //initialization of variables
-        editText=findViewById(R.id.editTextTextPersonName);
-        button=findViewById(R.id.button);
-        mic=findViewById(R.id.imageView);
+        editText = findViewById(R.id.editTextTextPersonName);
+        button = findViewById(R.id.button);
+        mic = findViewById(R.id.imageView);
 
 
-        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if (status!=TextToSpeech.ERROR){
+                if (status != TextToSpeech.ERROR) {
                     t1.setLanguage(Locale.US);
                 }
             }
@@ -43,7 +44,13 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                userToSpeak=editText.getText().toString();
+                userToSpeak = editText.getText().toString();
+                if (userToSpeak.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Please Enter Your Text", Toast.LENGTH_SHORT).show();
+                } else {
+                    t1.speak(userToSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                    Toast.makeText(getApplicationContext(), userToSpeak, Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
